1782959e442823de66b3a12b9202afcaaed35160
densinela <densinela@gmail.com>
2026-03-31 14:09:29 +0300

    functions has been added

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+function calculateTax(amount) {
+  if (amount === 0) return 0;
+  return amount * 0.1;
+}
 
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
 
+function findMaximum(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
+function isPalindrome(word) {
+  if (word.split("").length === 1 || word.split("") === "") {
+    return true;
+  }
+  const reversed = word.split("").reverse().join("");
 
+  if (reversed === word) {
+    return true;
+  }
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+  return false;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (discountPercentage / 100) * originalPrice;
+}
+
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
